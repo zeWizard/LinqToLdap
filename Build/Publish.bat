3ef6acb8d69a1793f76964a1eacac3b48f49dfce
zeWizard <ta.smith81@y7mail.com>
2018-05-31 16:31:54 +1000

    Add Builds and publish.

---
+++ Build/Publish.bat
@@ -0,0 +1,15 @@
+IF [%1] == [] (
+ SET APIKEY=""
+)
+
+IF NOT [%1] == [] (
+ SET APIKEY=%1%
+)
+
+del *.nupkg
+
+MSBuild.exe ..\LinqToLdap.sln /t:Clean /p:Configuration=Release /p:Platform="Any CPU"
+MSBuild.exe ..\LinqToLdap.sln /t:Build /p:Configuration=Release /p:Platform="Any CPU"
+
+nuget.exe pack ..\LinqToLdap\LinqToLdap.nuspec -Prop Configuration=Release
+nuget.exe push *.nupkg %APIKEY% -Source https://api.nuget.org/v3/index.json
