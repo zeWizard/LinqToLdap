f2a1d43aa92b2fdcc04357bcbdb64582fb4a3e26
zeWizard <ta.smith81@y7mail.com>
2018-06-01 10:27:25 +1000

    Updates to include LinqToLdap Contrib.

--- Build/Publish.bat
+++ Build/Publish.bat
@@ -11,5 +11,5 @@ del *.nupkg
 MSBuild.exe ..\LinqToLdap.sln /t:Clean /p:Configuration=Release /p:Platform="Any CPU"
 MSBuild.exe ..\LinqToLdap.sln /t:Build /p:Configuration=Release /p:Platform="Any CPU"
 
-nuget.exe pack ..\LinqToLdap\LinqToLdap.nuspec -Prop Configuration=Release
+nuget.exe pack ..\LinqToLdap\LinqToLdap.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget.exe push *.nupkg %APIKEY% -Source https://api.nuget.org/v3/index.json
